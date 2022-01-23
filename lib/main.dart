4eaca4daf35d5d8757104ce4142750f9016aa836
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-01-23 14:59:15 +0900

    Add a style of phone number with icons

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,28 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(width: 10.0),
+                    Text(
+                      '+82 010 1234 5678',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),

222718f57af92d0d2e914af00654e06013b27f80
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-01-23 14:19:39 +0900

    Add fonts example

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,20 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
+          child: Column(
             children: [
               CircleAvatar(
                 radius: 50.0,
-                backgroundColor: Colors.red,
                 backgroundImage: AssetImage('images/profile.png'),
+              ),
+              Text(
+                'Jongmin Park',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ],
           ),
